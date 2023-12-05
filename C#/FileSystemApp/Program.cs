91de7885add28274b3bda4aef78e626204ac6ab1
Victoria <victoria1orlova@gmail.com>
2023-12-05 19:26:49 +0100

    File system app refactoring

--- C#/FileSystemApp/Program.cs
+++ C#/FileSystemApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿Console.WriteLine("FileSystemApp");
-Directory.CreateDirectory("MyDirectory");
-var filePath = Path.Combine("MyDirectory", "MyFile.txt");
+
+string mainDir = "MyDirectory";
+Directory.CreateDirectory(mainDir);
+var filePath = Path.Combine(mainDir, "MyFile.txt");
 File.WriteAllText(filePath, "MyContent");
 for (int i = 1; i < 11; i++)
 {
-    Directory.CreateDirectory("My" + i.ToString());
+    string subDirPath = Path.Combine(mainDir,"My" + i.ToString());
+    Directory.CreateDirectory(subDirPath);
 }
